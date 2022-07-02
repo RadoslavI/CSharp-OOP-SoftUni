eeca47deb76a750c9e417462a83efc39d1be100a
Radoslav Ivanov <radogi001@gmail.com>
2022-07-02 13:51:27 +0300

    add Person example

---
+++ Inheritance - Exercise/1-Person/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public virtual int Age
+        {
+            get { return Age; }
+            
+            set { 
+                if (this.Age < 0)                
+                    this.Age = 0;                
+                else 
+                    this.Age = value;
+                }
+        }
+
+
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(String.Format("Name: {0}, Age: {1}",
+                                 this.Name,
+                                 this.Age));
+
+            return stringBuilder.ToString();
+        }
+
+
+    }
+}
