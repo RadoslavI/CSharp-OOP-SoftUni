8bd87494bac9648d2e37b819f0e970b13cb0336d
Radoslav Ivanov <radogi001@gmail.com>
2022-07-02 16:18:38 +0300

    added restaurant exercise

---
+++ Inheritance - Exercise/5-Restaurant/Beverage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Beverage : Food
+    {
+        public Beverage(string name, decimal price, double milliliters) : base(name, price)
+        {
+            Milliliters = milliliters;
+        }
+
+        public double Milliliters { get; set; }
+
+        
+    }
+}
---
+++ Inheritance - Exercise/5-Restaurant/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //var cake = new Cake("torta", 50, 200, 450);
+            //System.Console.WriteLine(cake.Name);
+            //System.Console.WriteLine(cake.Grams);
+            //System.Console.WriteLine(cake.Price);
+            //System.Console.WriteLine(cake.Calories);
+        }
+    }
+}
