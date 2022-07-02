d46ee98cf98a577323602e5a4a6e360219a7c26f
Radoslav Ivanov <radogi001@gmail.com>
2022-07-02 15:38:11 +0300

    added NeedForSpeed exercise

---
+++ Inheritance - Exercise/4-NeedForSpeed/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+
+            //var sportsCar = new RaceMotorcycle(200, 100);
+            //System.Console.WriteLine(sportsCar.Fuel);
+            //sportsCar.Drive(20);
+            //System.Console.WriteLine(sportsCar.Fuel);
+
+        }
+    }
+}
---
+++ Inheritance - Exercise/4-NeedForSpeed/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public double FuelConsumption  { get; set; }
+
+        public virtual double DefaultFuelConsumption { get; set; } = 1.25;
+
+
+        public double Fuel { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            Fuel = Fuel - (kilometers * DefaultFuelConsumption);
+        }
+    }
+}
