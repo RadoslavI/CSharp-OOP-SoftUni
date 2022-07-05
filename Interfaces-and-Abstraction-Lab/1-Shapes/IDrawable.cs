5465164a40bba24b7b76df8312225d18ac97f9f4
Radoslav Ivanov <radogi001@gmail.com>
2022-07-05 23:27:53 +0300

    added shapes exercise

---
+++ Interfaces-and-Abstraction-Lab/1-Shapes/IDrawable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Shapes
+{
+    public interface IDrawable
+    {
+        void Draw();
+    }
+}
