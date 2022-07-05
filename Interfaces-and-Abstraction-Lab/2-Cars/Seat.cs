a1b7491f5a8af43be9b2028d27004adc12f0191e
Radoslav Ivanov <radogi001@gmail.com>
2022-07-05 23:56:39 +0300

    added Cars exercise

---
+++ Interfaces-and-Abstraction-Lab/2-Cars/Seat.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    internal class Seat : ICar
+    {
+        private string v1;
+        private string v2;
+
+        public Seat(string v1, string v2)
+        {
+            Model = v1;
+            Color = v2;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{Color} Seat {Model} ";
+        }
+    }
+}
---
+++ Interfaces-and-Abstraction-Lab/2-Cars/Tesla.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : IElectricCar, ICar
+    {
+        private string v1;
+        private string v2;
+        private int v3;
+
+        public Tesla(string v1, string v2, int v3)
+        {
+            Model = v1;
+            Color = v2;
+            Battery = v3;
+        }
+
+        public int Battery { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} Tesla {Model} ";
+        }
+    }
+}
