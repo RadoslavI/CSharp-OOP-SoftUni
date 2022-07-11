f43bf4d79eb0741546c0392d90808b3b2efb6bd8
Radoslav Ivanov <radogi001@gmail.com>
2022-07-11 23:56:38 +0300

    commit

--- Exceptions - Lab/2-EnterNumbers/Program.cs
+++ Exceptions - Lab/2-EnterNumbers/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _2_EnterNumbers
 {
@@ -6,7 +7,51 @@ namespace _2_EnterNumbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int start = 1;
+            int end = 100;
+
+            List<int> nums = new List<int>();
+            do
+            {
+                try
+                {
+                    nums.Add(ReadNumber(nums, start, end));
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } while (nums.Count < 10);
+
+            Console.WriteLine(string.Join(", ", nums));
+        }
+
+        private static int ReadNumber(List<int> nums, int start, int end)
+        {
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out _))
+            {
+                throw new FormatException("Invalid Number!");
+            }
+
+            int num = int.Parse(input);
+
+            if (nums.Count > 0 && num <= nums[^1])
+            {
+                throw new ArgumentException($"Your number is not in range {nums[^1]} - 100!");
+            }
+
+            if (num <= start || num >= end)
+            {
+                throw new ArgumentException($"Your number is not in range {start} - 100!");
+            }
+
+            return num;
         }
     }
 }
