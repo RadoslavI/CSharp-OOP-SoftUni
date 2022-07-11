53fb2ae1a742ebf3016358fc80794325d9dde70d
Radoslav Ivanov <radogi001@gmail.com>
2022-07-11 23:55:21 +0300

    added Enter Numbers Exercise

---
+++ Exceptions - Lab/2-EnterNumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2_EnterNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
