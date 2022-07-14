a5a19d9385406e5183a141a210e5a2c525c2bdee
Radoslav Ivanov <radogi001@gmail.com>
2022-07-14 17:30:17 +0300

    initial commit

---
+++ Reflection/2-High-Quality-Mistake/Spy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_High_Quality_Mistake
+{
+    internal class Spy
+    {
+    }
+}
---
+++ Reflection/2-High-Quality-Mistake/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2_High_Quality_Mistake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
