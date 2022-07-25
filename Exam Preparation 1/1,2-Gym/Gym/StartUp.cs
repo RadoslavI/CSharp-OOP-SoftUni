41ff429fe8329eab4310cd1f4fec06d8aed894ba
Radoslav Ivanov <radogi001@gmail.com>
2022-07-25 14:46:13 +0300

    initial commit

---
+++ Exam Preparation 1/1,2-Gym/Gym/StartUp.cs
@@ -0,0 +1,24 @@
+﻿namespace Gym
+{
+    using Gym.Core;
+    using Gym.Core.Contracts;
+    using Gym.Models.Athletes;
+    using Gym.Models.Equipment;
+    using Gym.Models.Gyms;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            //BoxingGym gym = new BoxingGym("Boxing gym");
+            //gym.AddAthlete(new Boxer("Joro", "dokrai", 1));
+            //gym.Exercise();
+            //gym.AddEquipment(new BoxingGloves());
+            //System.Console.WriteLine(gym.GymInfo());
+
+            //// Don't forget to comment out the commented code lines in the Engine class!
+            IEngine engine = new Engine();
+            engine.Run();
+        }
+    }
+}
